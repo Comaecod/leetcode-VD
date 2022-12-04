e75ec8011acba22471034a82ea5a5dc32553b46f
comaecod <vishnuthecoder@gmail.com>
2022-12-04 14:37:00 +0530

    Create Spring-Initializr Repo

---
+++ src/test/java/com/springbootlearning/visdhe/VisDheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootlearning.visdhe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisDheApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
